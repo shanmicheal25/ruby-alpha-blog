8949638dd09236868296fd6904a0563ba790fd76
shankar <shanmicheal25@gmail.com>
2020-10-03 23:06:16 +0800

    created crud operation manually

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,66 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show 
-    byebug
+    #byebug
+    #@article = Article.find(params[:id])
+  end
+
+  def index 
+    @articles = Article.all
+  end
+
+  def new 
+    @article = Article.new
+  end
+
+  def edit 
+    #@article = Article.find(params[:id])
+  end
+
+  def create 
+    #byebug
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was creqetd successfully."
+      redirect_to @article 
+    else
+      render 'new'
+    end
+    #render plain: @article.inspect
+    ## redirect_to article_path(@article) ## commonly used
+    ## redirect_to @article  ## shortend path.
+    #render plain: params[:article]
+  end
+
+  def update
+    #@article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article 
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    # @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] = "Article was deleted successfully."
+      redirect_to @article 
+    end
+  end
+
+
+  private 
+
+  def set_article 
     @article = Article.find(params[:id])
   end
+
+  def article_params 
+    params.require(:article).permit(:title, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "page#home"
   get "about" => "page#about"
 
-  resources :articles, only: [:show]
+  resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
